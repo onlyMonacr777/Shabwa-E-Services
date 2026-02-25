127e7c474a066ea9bb848f83887d2c1365ed030d
Mona <monaw02815@gmail.com>
2026-02-25 08:24:55 +0300

    Initial design for admin dashboard with Services Screen and Users Screen

---
+++ lib/screens/admin/admin_services_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AdminServicesScreen extends StatelessWidget {
+  const AdminServicesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('إدارة الخدمات', style: TextStyle(color: Colors.white)),
+    );
+  }
+}
---
+++ lib/screens/admin/admin_users_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AdminUsersScreen extends StatelessWidget {
+  const AdminUsersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('إدارة المواطنين', style: TextStyle(color: Colors.white)),
+    );
+  }
+}
