377d598e3206fda3a56283c09bfaa5cb5f80512e
Mona <monaw02815@gmail.com>
2026-02-25 08:21:37 +0300

    Initial design admin dashboard with Requests Screen

---
+++ lib/screens/admin/admin_requests_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AdminRequestsScreen extends StatelessWidget {
+  const AdminRequestsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('إدارة الطلبات', style: TextStyle(color: Colors.white)),
+    );
+  }
+}
