d3953ce98cb7f7e2733c6ebb41300c6cf06ede88
Mona <monaw02815@gmail.com>
2026-05-07 14:30:52 +0300

    create requests data mock

---
+++ lib/data/mock/requests_data.dart
@@ -0,0 +1,20 @@
+final requestsData = [
+  {
+    'userName': 'أحمد محمد',
+    'serviceName': 'استخراج جواز سفر',
+    'status': 'pending',
+    'date': '2026/05/07',
+  },
+  {
+    'userName': 'محمد علي',
+    'serviceName': 'تجديد جواز سفر',
+    'status': 'approved',
+    'date': '2026/05/06',
+  },
+  {
+    'userName': 'سالم خالد',
+    'serviceName': 'بطاقة شخصية',
+    'status': 'rejected',
+    'date': '2026/05/05',
+  },
+];
